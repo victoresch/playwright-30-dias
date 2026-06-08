6d7e916d1ac98619823d48ec88e1241977b24b83
Victor Estupinan <victoresch@correo.com>
2026-06-08 17:37:24 -0500

    se agregan configuraciónes de tiempo de ejecución

---
+++ tests/training.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test('login sauce demo', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await page.getByRole('textbox',{name:'Username'}).fill('standard_user')
+    await page.getByRole('textbox',{name:'Password'}).fill('secret_sauce')
+    await page.getByRole('button',{name:'Login'}).click()
+
+})
